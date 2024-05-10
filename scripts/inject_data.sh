942d8d058603b102b7bb8a80ef0be747cbf09067
fernaldya <76620982+fernaldya@users.noreply.github.com>
2024-05-10 18:07:44 +0700

    moved script to scripts folder

--- scripts/inject_data.sh
+++ scripts/inject_data.sh
@@ -7,7 +7,7 @@ USER="postgres"
 DB="salary"
 SCHEMA="salary_data"
 TABLE="salaries"
-FILE="/psql_data/salaries.csv"
+FILE="'/psql_data/salaries.csv'"
 
 # Data insertion
 psql -h $HOST -p $PORT -U $USER -d $DB <<-EOSQL
